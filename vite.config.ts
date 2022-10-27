ef20e13c87c1c6174b3d3c3eee1f5feb4d1f43cd
weirui88888 <ruiwei88888@163.com>
2022-10-27 10:58:27 +0800

    ci: 🔧 set server in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,12 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: true, // host设置为true才可以使用network的形式，以ip访问项目
+    port: 6800, // 端口号
+    open: true, // 自动打开浏览器
+    cors: true, // 跨域设置允许
+    strictPort: true // 如果端口已占用直接退出
   }
 })
