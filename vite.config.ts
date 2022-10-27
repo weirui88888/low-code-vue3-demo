9dbbb59c7fea1d868b7e2de5fc5bc6cb7d9d31e8
weirui88888 <ruiwei88888@163.com>
2022-10-27 11:05:45 +0800

    ci: 🔧 set build in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,5 +17,17 @@ export default defineConfig({
     open: true, // 自动打开浏览器
     cors: true, // 跨域设置允许
     strictPort: true // 如果端口已占用直接退出
+  },
+  build: {
+    reportCompressedSize: false,
+    chunkSizeWarningLimit: 2000,
+    assetsDir: 'static/assets',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'static/js/[name]-[hash].js',
+        entryFileNames: 'static/js/[name]-[hash].js',
+        assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
+      }
+    }
   }
 })
