373cb8f09dcf7e58c2c249fb52df1af9b6710841
weirui88888 <ruiwei88888@163.com>
2022-11-14 16:01:04 +0800

    feat: 🌸 update some feature

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
-  <div class="membership" @click="click" v-text="text"></div>
+  <div class="membership">
+    {{ appConfig.title }}
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-console.log(import.meta.env)
-const click = () => {
-  alert('hello')
-}
-const text = ref('hello')
+import appConfig from '@/app.config.json'
 </script>
 
 <style scoped lang="scss">
 .membership {
-  font: {
-    size: 20px;
-    weight: bolder;
-  }
-
-  color: #90ee90;
+  width: 100%;
+  height: 200px;
+  color: v-bind('appConfig.color');
+  background-position: 0 0;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-image: v-bind('appConfig.backgroundImage');
 }
 </style>
